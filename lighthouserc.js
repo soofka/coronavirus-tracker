5eb0cd45454dc86e648e1c4ea53d3db3da1211f7
Jakub Sowinski <Jakub.Sowinski@stepstone.com>
2020-03-29 20:37:51 +0200

    chore: temporarily disabled lighthouse tests

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,7 +3,7 @@ module.exports = {
     assert: {
       preset: 'lighthouse:recommended',
       assertions: {
-        'uses-text-compression': 'off',
+        // 'uses-text-compression': 'off',
         'redirects-http': 'off',
         'uses-http2': 'off',
       },
