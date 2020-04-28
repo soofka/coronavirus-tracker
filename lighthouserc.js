bb4bee7c2de49e2e71ec3387fbc9bb90774cc7da
Jakub Sowinski <Jakub.Sowinski@stepstone.com>
2020-04-28 21:07:12 +0200

    chore: attempted to run tests during CI/CD 32

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,10 @@
 module.exports = {
   ci: {
+    collect: {
+      settings: {
+        chromeFlags: '--disable-gpu --no-sandbox --headless',
+      },
+    },
     assert: {
       preset: 'lighthouse:recommended',
       assertions: {
