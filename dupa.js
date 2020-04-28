ed26a3d0089bd8350d9793073bfb650f21493505
Jakub Sowinski <Jakub.Sowinski@stepstone.com>
2020-04-28 18:13:42 +0200

    chore: attempted to run tests during CI/CD 12

--- dupa.js
+++ dupa.js
@@ -1,2 +1,9 @@
+const path = require('path');
 const shell = require('shelljs');
-shell.exec('dupa.sh');
+// const fetch = require('node-fetch');
+
+// fetch('https://raw.githubusercontent.com/GoogleChrome/chrome-launcher/v0.8.0/scripts/download-chrome.sh')
+//   .then(response => response.text())
+//   .then(script => )
+
+shell.exec(path.resolve('./dupa.sh'));
