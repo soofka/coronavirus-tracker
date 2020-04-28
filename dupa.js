abe9695058a51da04c7147b818c5745b68279567
Jakub Sowinski <Jakub.Sowinski@stepstone.com>
2020-04-28 19:46:38 +0200

    chore: attempted to run tests during CI/CD 20

--- dupa.js
+++ dupa.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 
 console.log('WUT', process.env.CHROME_PATH);
-console.log('egzystuje', fs.existsSync(process.env.CHROME_PATH));
+console.log('egzystuje', fs.existsSync('node_modules'));
