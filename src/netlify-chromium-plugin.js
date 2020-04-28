d637b313b709301be6ac693e4787ea3339510bc3
Jakub Sowinski <Jakub.Sowinski@stepstone.com>
2020-04-28 23:44:42 +0200

    chore: attempted to run tests during CI/CD 35.2

--- src/netlify-chromium-plugin.js
+++ src/netlify-chromium-plugin.js
@@ -1,52 +1,23 @@
-const fs = require('fs');
-
-// const ENV_VARS = {
-//   CHROME_PATH: '/opt/build/repo/node_modules/chromium/lib/chromium',
-//   DISPLAY: ':99.0',
-// };
-
 const NetlifyChromiumPlugin = {
   name: 'netlify-chromium-plugin',
   onInstall: () => {
-    // Object.keys(ENV_VARS).forEach((key) => {
-    //   if (!process.env[key]) {
-    //     console.log(`Setting environmental variable ${key} = ${ENV_VARS[key]}`);
-    //     process.env[key] = ENV_VARS[key];
-    //   }
-    // });
+    console.log('[NetlifyChromiumPlugin]: Installing Chromium');
 
-    if (!process.env.CHROME_PATH || !fs.existsSync(process.env.CHROME_PATH)) {
-      console.log('Installing chromium and chrome-launcher');
+    try {
       require('child_process').execSync(
-        'npm install chromium chrome-launcher',
+        'npm list chromium || npm install chromium',
         {
           cwd: process.cwd(),
           stdio: 'inherit',
         },
       );
 
-      console.log('Testing started');
-
       const { path } = require('chromium');
-      console.log(`Setting CHROME_PATH (${process.env.CHROME_PATH}) to ${path}`);
       process.env.CHROME_PATH = path;
 
-      const { Launcher, launch, killAll } = require('chrome-launcher');
-      console.log('Chrome installations:', Launcher.getInstallations());
-
-      return launch({ startingUrl: 'https://google.com' })
-        .then(({ port, pid, process }) => {
-          console.log(`Chrome is up (port: ${port}, pid: ${pid}, process: ${JSON.stringify(process)})`);
-          killAll().then(() => {
-            console.log('Killed all Chrome instances');
-            console.log('Testing success');
-            // process.exit(0);
-          });
-        })
-        .catch((error) => {
-          console.log('Testing failure:', error);
-          // process.exit(1);
-        });
+      console.log('[NetlifyChromiumPlugin]: Chromium installation finished with SUCCESS', process.env.CHROME_PATH);
+    } catch(error) {
+      console.log('[NetlifyChromiumPlugin]: Chromium installation finished with FAILURE', error);
     }
   },
 };
